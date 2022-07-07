77c9d3c710c462c7dc21fba7b873692a544d74dc
EySoul <ijasolzzz@gmail.com>
2022-07-07 14:35:34 +0300

    right now

---
+++ Assets/scripts/move.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public bool onGround;
+    public Transform GroundCheck;
+    private void Update() {
+        
+    }
+    void CheckingGround()
+    {
+        onGround = 
+    }
+
+}
