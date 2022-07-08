6877863a9a1165dfa21a05d86c16db8e74cec62b
EySoul <ijasolzzz@gmail.com>
2022-07-08 11:01:51 +0300

    New new Move

--- Assets/scripts/HeroAndCamara/move.cs
+++ Assets/scripts/HeroAndCamara/move.cs
@@ -6,15 +6,14 @@ public class move : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Rigidbody2D rb_Platforms;
-    public GameObject platforms;
-    public Transform Point;
-    public Transform Hero;
-    public float jumpForce = 15;
-    public bool onGround = false;
-    public Transform GroundCheck;
-    public LayerMask ground;
-    public LayerMask Rect;
-    public float moveSpead = 40;
+    [SerializeField] private GameObject platforms;
+    [SerializeField] private Transform Hero;
+    [SerializeField] private float jumpForce = 15;
+    private bool onGround = false;
+    [SerializeField] private Transform GroundCheck;
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private LayerMask Rect;
+    [SerializeField] private float moveSpead = 40;
     private float Rad = 0.4f;
     public bool CheckTrigger;
     private void Start()
