c3a58e22806d620d8b39608fdca6bcdeaa3acc2f
Egor Frolov <egorfrolov2019.ef@gmail.com>
2022-07-09 07:58:20 +0300

    redesign and add mask for anything

--- Assets/scripts/StartIntro/WaitSomeTime.cs
+++ Assets/scripts/StartIntro/WaitSomeTime.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class WaitSomeTime : MonoBehaviour
 {
     public float waitSeconds;
     public string nextScene;
+    public TextMeshProUGUI textField;
+    public Slider sliderField;
     
     // Start is called before the first frame update
     void Start()
@@ -22,7 +26,22 @@ public class WaitSomeTime : MonoBehaviour
 
     IEnumerator Waiter()
     {
-        yield return new WaitForSeconds(waitSeconds);
-        SceneManager.LoadScene(nextScene);
+        AsyncOperation async = SceneManager.LoadSceneAsync(nextScene);
+        async.allowSceneActivation = false;
+
+        while (!async.isDone)
+        {
+            textField.text = async.progress * 100 + "%";
+            sliderField.value = async.progress;
+
+            if (async.progress >= 0.9f)
+            {
+                yield return new WaitForSeconds(waitSeconds);
+                async.allowSceneActivation = true;
+                break;
+            }
+            
+            yield return null;
+        }
     }
 }
